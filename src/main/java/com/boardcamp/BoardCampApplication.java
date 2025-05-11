eb4a02fa5fc896fba4689787514b10674021dfa5
alessandrocdiasbr <acdias83@gmail.com>
2025-05-10 22:26:53 -0300

    criado bases do projeto

---
+++ src/main/java/com/boardcamp/BoardCampApplication.java
@@ -0,0 +1,13 @@
+package com.boardcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardCampApplication.class, args);
+	}
+
+}
